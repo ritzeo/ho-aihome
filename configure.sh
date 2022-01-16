5ddc6bf6f4a8cc9ac20f5d22b7da26e4142a9e4b
ritzeo <96355819+ritzeo@users.noreply.github.com>
2022-01-16 19:24:24 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -30,7 +30,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "decryption": "none"
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "tcp"
             }
         }
     ],
